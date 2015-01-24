d84eb1bedb24695ff99d170f08810f078382dd26
Markgasus <markgasus@gmail.com>
2015-01-24 15:08:57 -0500

    Change theme and remove random.choice This is because we only have one theme song now

--- toontown/distributed/ToontownClientRepository.py
+++ toontown/distributed/ToontownClientRepository.py
@@ -207,8 +207,8 @@ class ToontownClientRepository(OTPClientRepository.OTPClientRepository):
         self.sendSetAvatarIdMsg(0)
         self.clearFriendState()
         if self.music == None and base.musicManagerIsValid:
-            themeList = ('phase_3/audio/bgm/tti_theme.ogg', 'phase_3/audio/bgm/tti_theme_2.ogg')
-            self.music = base.musicManager.getSound(random.choice(themeList))
+            music = ('phase_3/audio/bgm/ttf_theme.ogg')
+            self.music = base.musicManager.getSound((music))
             if self.music:
                 self.music.setLoop(1)
                 self.music.setVolume(0.9)
