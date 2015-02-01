41064aea62b6424acdf42ed30cd4271b945a7fa6
Chan <chandler14362@gmail.com>
2015-01-31 22:43:07 -0500

    The DistributedBrutalVPElevatorAI now tells the LobbyManagerAI to load in a brutal boss

--- toontown/building/DistributedBrutalVPElevatorAI.py
+++ toontown/building/DistributedBrutalVPElevatorAI.py
@@ -2,4 +2,9 @@ from toontown.building.DistributedVPElevatorAI import DistributedVPElevatorAI
 
 
 class DistributedBrutalVPElevatorAI(DistributedVPElevatorAI):
-    pass
+    def sendAvatarsToDestination(self, avIdList):
+        if len(avIdList) > 0:
+            bossZone = self.bldg.createBossOffice(avIdList, isBrutal=True)
+            for avId in avIdList:
+                if avId:
+                    self.sendUpdateToAvatarId(avId, 'setBossOfficeZoneForce', [bossZone])
