54af4c0cba8277b5c86bb7dc609849df407a62c3
Chan <chandler14362@gmail.com>
2015-02-02 03:54:40 -0500

    Unites are now disabled in the brutal CFO

--- toontown/suit/DistributedBrutalCashbotBoss.py
+++ toontown/suit/DistributedBrutalCashbotBoss.py
@@ -3,3 +3,13 @@ from toontown.suit.DistributedCashbotBoss import DistributedCashbotBoss
 
 class DistributedBrutalCashbotBoss(DistributedCashbotBoss):
     notify = directNotify.newCategory('DistributedBrutalCashbotBoss')
+
+    def announceGenerate(self):
+        DistributedCashbotBoss.announceGenerate(self)
+
+        base.localAvatar.setCanUseUnites(False)
+
+    def disable(self):
+        DistributedCashbotBoss.disable(self)
+
+        base.localAvatar.setCanUseUnites(True)
