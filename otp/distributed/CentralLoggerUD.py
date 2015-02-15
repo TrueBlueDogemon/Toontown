50066c920cb2680f71ea4c859daa59b1604805d5
enigmeow <jsiegel@stormbirds.org>
2015-02-15 08:35:26 -0600

    make it build

--- otp/distributed/CentralLoggerUD.py
+++ otp/distributed/CentralLoggerUD.py
@@ -1,13 +1,15 @@
 from direct.directnotify import DirectNotifyGlobal
+from direct.distributed.DistributedObjectGlobalUD import DistributedObjectGlobalUD
 from direct.distributed.DistributedObjectUD import DistributedObjectUD
 
 class CentralLoggerUD(DistributedObjectUD):
-    notify = DirectNotifyGlobal.directNotify.newCategory("CentralLoggertUD")
+    notify = DirectNotifyGlobal.directNotify.newCategory("CentralLoggerUD")
 
     def announceGenerate(self):
         DistributedObjectGlobalUD.announceGenerate(self)
 
     def sendMessage(self, category, description, sender, receiver):
+        print ['sendMessage', category, description, sender, receiver]
         self.air.writeServerEvent(category, sender, receiver, description)
 
     def logAIGarbage(self):
