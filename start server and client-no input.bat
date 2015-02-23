468f08f8df4860cdaa65c0a19709a7ec9a1074cf
judge2020 <hunterray55@hotmail.com>
2015-02-23 17:49:50 -0500

    batch files for easier development

---
+++ start server and client-no input.bat
@@ -0,0 +1,4 @@
+cd astron/win32
+start start_all_no_input
+cd ../../win32
+start start_game_localhost_no_input
---
+++ start server and client.bat
@@ -0,0 +1,8 @@
+cd astron
+cd win32
+start start_all
+cd ..
+cd ..
+cd win32
+start start_game_localhost
+exit
