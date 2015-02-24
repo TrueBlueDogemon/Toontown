915d138272a4f39cc22e58c779a4015699729d1c
Xenofied <Xenofied@users.noreply.github.com>
2015-02-24 14:19:28 -0600

    ToontownGlobals:Add BrutalFactory Zone ID

--- toontown/toonbase/ToontownGlobals.py
+++ toontown/toonbase/ToontownGlobals.py
@@ -192,6 +192,7 @@ SellbotHQ = 11000
 SellbotLobby = 11100
 SellbotFactoryExt = 11200
 SellbotFactoryInt = 11500
+SellbotBrutalFactoryInt = 11600
 CashbotHQ = 12000
 CashbotLobby = 12100
 CashbotMintIntA = 12500
@@ -300,6 +301,7 @@ FT_Arm = 'arm'
 FT_Torso = 'torso'
 factoryId2factoryType = {MockupFactoryId: FT_FullSuit,
  SellbotFactoryInt: FT_FullSuit,
+ SellbotBrutalFactoryInt: FT_FullSuit,
  LawbotOfficeInt: FT_FullSuit}
 StreetNames = TTLocalizer.GlobalStreetNames
 StreetBranchZones = StreetNames.keys()
